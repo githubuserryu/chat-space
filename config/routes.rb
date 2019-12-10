996f29b5a92f7e42d912c2976a6781686703454e
githubuserryu <ootaryunosuke@gmail.com>
2019-12-10 21:48:40 +0900

    signdup-user3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'groups/index'
   devise_for :users
-  root "messages#index"
+  root "groups#index"
+  resources :users, only: [:edit, :update, :destroy]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :message,only: [:index]
+  end 
 end

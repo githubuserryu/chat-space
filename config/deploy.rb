512ef4da675fb418819bb31a96d4427588efa0e2
githubuserryu <ootaryunosuke@gmail.com>
2019-12-20 21:30:27 +0900

    setting-7

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,11 +34,6 @@ end
 
 set :linked_files, %w{ config/secrets.yml }
 
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
 
   desc 'upload secrets.yml'
   task :upload do
